2e68949155806ed7de9341012b53551f831a10fc
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-02-11 15:35:26 +0500

    multipleimagewss

--- Readimages/cv.py
+++ Readimages/cv.py
@@ -1,13 +1,32 @@
 import cv2
+import numpy as np
 
 
 image1 = cv2.imread("image.jpg.png")  
 image2 = cv2.imread("capture (1).png")  
 
   
-cv2.imshow("Github Image", image1)
-cv2.imshow("React Image", image2)
+# cv2.imshow("Github Image", image1)
+# cv2.imshow("React Image", image2)
 
 cv2.waitKey(100000)
 
 cv2.destroyAllWindows()
+
+# resize the image
+
+img1= cv2.imread('IMG_0886.JPG')
+res_img = cv2.resize(img1,(100,200))
+# cv2.imshow("resize the image" , res_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows(0)
+
+# show multiple images in one frame 
+
+
+img2= cv2.imread('IMG_0886.JPG')
+res_img = cv2.resize(img1,(300,700))
+multiple_img = np.hstack((res_img,res_img))
+cv2.imshow("resize the image" , multiple_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows(0)
