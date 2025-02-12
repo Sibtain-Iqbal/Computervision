6323390fa3352ee9a4791044bda64984d58ba184
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-02-12 12:04:34 +0500

    ALL IMREAD FUNCTION OF OPENCV

---
+++ Readimages/Imread_Func.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+
+img0 = cv2.imread(r"D:\Computervision\Readimages\IMG_0886.JPG",cv2.IMREAD_COLOR) #load the olor image 
+img1 = cv2.imread(r"D:\Computervision\Readimages\IMG_0886.JPG",cv2.IMREAD_GRAYSCALE)  #the image will show black
+caps = cv2.imread(r"D:\Computervision\Readimages\IMG_0886.JPG",cv2.IMREAD_UNCHANGED)  #the image will show black
+h =np.hstack((img0,caps))
+cap = cv2.resize(h,(500,700))
+cv2.imshow("All Read Functions",cap)
+cv2.waitKey(0)
+cv2.destroyAllWindows(0)
