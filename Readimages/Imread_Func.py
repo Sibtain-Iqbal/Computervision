b2abd14fef8ccff37cac9b69640b2e835e0e1fda
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-02-12 12:31:04 +0500

    apply allimread function of open cv

--- Readimages/Imread_Func.py
+++ Readimages/Imread_Func.py
@@ -5,7 +5,12 @@ import numpy as np
 img0 = cv2.imread(r"D:\Computervision\Readimages\IMG_0886.JPG",cv2.IMREAD_COLOR) #load the olor image 
 img1 = cv2.imread(r"D:\Computervision\Readimages\IMG_0886.JPG",cv2.IMREAD_GRAYSCALE)  #the image will show black
 caps = cv2.imread(r"D:\Computervision\Readimages\IMG_0886.JPG",cv2.IMREAD_UNCHANGED)  #the image will show black
-h =np.hstack((img0,caps))
+gray = cv2.imread("grayscale.png",cv2.IMREAD_GRAYSCALE)
+
+
+
+h =np.hstack((img0,img0))
+
 cap = cv2.resize(h,(500,700))
 cv2.imshow("All Read Functions",cap)
 cv2.waitKey(0)
