2c1d20896989bcf1841437de1edc2e95b08dc692
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-02-16 16:21:26 +0500

    make a box around face using open cv
    i will make a simple dection box and make a box around face using open Cv

--- detectionBox_Image/Box.py
+++ detectionBox_Image/Box.py
@@ -2,9 +2,9 @@ import cv2
 
 
 img = cv2.imread("hello.png")
-box = cv2.rectangle(img = img , pt1 = (200,300),pt2=(100,300),color(0,0,255),lineType=16,
-    thickness = 10 )
+res_img  = cv2.resize(img,(500,700))
+box = cv2.rectangle(img =res_img , pt1 = (150,50),pt2=(350,350),color = (10,10,255),lineType=16,thickness = 3 )
 
 cv2.imshow("detection box",box)
-cv2.waitKey(5000)
-cv2.destroyAllWindows(5000)
+cv2.waitKey(0)
+cv2.destroyAllWindows(0)
