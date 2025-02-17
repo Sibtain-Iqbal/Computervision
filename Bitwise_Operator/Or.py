f3fcbe18ff5c71182eb368ca159c2ded1675eb4f
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-02-17 11:22:25 +0500

    Or operator perfom on img

---
+++ Bitwise_Operator/Or.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img1 = cv2.imread(r"D:\Computervision\detectionBox_Image\istockphoto-505370599-1024x1024.jpg")
+img2 = cv2.imread(r"D:\Computervision\detectionBox_Image\hello.png")
+
+
+res = cv2.resize(img1,(500,500))
+res2 = cv2.resize(img2,(500,500))
+
+ands = cv2.bitwise_or(res,res2)
+
+h = np.hstack((res,res2,ands))
+
+cv2.imshow("Or oprtator perfom" , h)
+cv2.waitKey(0)
+cv2.destroyAllWindows(0)
