18bccd18f050a51cb68fb277ade107047dc04f68
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-02-19 09:34:20 +0500

    Update scale.py

--- Scaling_and_rotating/scale.py
+++ Scaling_and_rotating/scale.py
@@ -1,14 +1,15 @@
 import cv2
-
+import numpy as np
 img = cv2.imread(r"D:\Computervision\detectionBox_Image\hello.png")
 
 res_img = cv2.resize(img,(340,500))
 
 h,w = res_img.shape[0],res_img.shape[1]
 
-rotate_img = cv2.getRotationMatrix2D((w/2 , h/2),45,1)
+rotate_img = cv2.getRotationMatrix2D((w/2 , h/2),60,1)
 l = cv2.warpAffine(res_img,rotate_img,(w,h))
-cv2.imshow(" rotate images",l)
-cv2.imshow("orignal image",res_img)
+merge = np.hstack((res_img,l))
+cv2.imshow(" rotate images",merge)
+# cv2.imshow("orignal image",res_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows
