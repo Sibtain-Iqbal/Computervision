9c8442111a6c5bd8ee7b4feb33d7626f479c877f
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-02-19 09:31:35 +0500

    Update scale.py

--- Scaling_and_rotating/scale.py
+++ Scaling_and_rotating/scale.py
@@ -6,8 +6,9 @@ res_img = cv2.resize(img,(340,500))
 
 h,w = res_img.shape[0],res_img.shape[1]
 
-rotate_img = cv2.getRotationMatrix2D((w/2 , h/2),90,1)
+rotate_img = cv2.getRotationMatrix2D((w/2 , h/2),45,1)
 l = cv2.warpAffine(res_img,rotate_img,(w,h))
-cv2.imshow("scale and rotate images",l)
+cv2.imshow(" rotate images",l)
+cv2.imshow("orignal image",res_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows
